1cc902db3820abadef90a70aaa7af21066a66247
houri314 <phucjaki@gmail.com>
2022-10-21 11:19:38 +0700

    app icon

--- src/misc.c
+++ src/misc.c
@@ -45,8 +45,14 @@ Config getGameConfig() {
 void initGame() {
 	if (conf.vsync) SetConfigFlags(FLAG_VSYNC_HINT);
 	if (conf.targetFPS>0) SetTargetFPS(conf.targetFPS);
+
 	ChangeDirectory(GetApplicationDirectory());
+
 	InitWindow(conf.ww, conf.wh, gameName);
 	InitAudioDevice();
 	SetMasterVolume(conf.volume);
+
+	Image icon = LoadImage("res/icon.png");
+	SetWindowIcon(icon);
+	UnloadImage(icon);
 }
